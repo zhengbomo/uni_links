a9089f00c65d15a93cf4b9b75f1042c0589ab1bf
zhengbomo <zhengbomo@hotmail.com>
2023-12-12 18:37:00 +0800

    add macos support

---
+++ uni_links/macos/Classes/UniLinksPlugin.h
@@ -0,0 +1,9 @@
+#import <FlutterMacOS/FlutterMacOS.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UniLinksPlugin : NSObject <FlutterPlugin>
++ (instancetype)sharedInstance;
+@end
+
+NS_ASSUME_NONNULL_END
